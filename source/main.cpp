4b92dcca0dc9470ca2fcd3fb50815f7ac8fe3e11
toeger <toeger@anonymouse.me>
2018-10-25 18:38:25 +0200

    Enabled drift style.

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,13 @@
 #include <QApplication>
 
+#include "driftuistyle.h"
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
 	QApplication app{argc, argv};
 	MainWindow mw;
+	DriftUIStyle style;
+	mw.setStyle(&style);
 	mw.show();
 	return app.exec();
 }
