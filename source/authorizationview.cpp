b1eeaa0747cbf521b6f6b2172581665dcfde0219
Piotr Kocia <p.kocia@wp.pl>
2018-11-10 17:26:49 +0100

    Updated to newest rpp

--- source/authorizationview.cpp
+++ source/authorizationview.cpp
@@ -7,28 +7,29 @@
 #include <QDebug>
 #include <QUrlQuery>
 
-AuthorizationView::AuthorizationView(QWidget *parent) : QWebEngineView(parent) {
+AuthorizationView::AuthorizationView(QWidget* parent) : QWebEngineView(parent) {
     connect(this, &AuthorizationView::urlChanged, this, &AuthorizationView::onURLChanged);
 }
 
-void AuthorizationView::onURLChanged(QUrl const &url) {
+void AuthorizationView::onURLChanged(QUrl const& url) {
     if (url.toString(QUrl::RemoveQuery) == discord::url::redirect) {
         QUrlQuery query(url);
         if (query.hasQueryItem("code")) {
             rpp::String code = query.queryItemValue("code").toStdString();
-            rpp::Request req(discord::url::token.toStdString());
             rpp::Body body;
-            body.add({rpp::BodyItem{"client_id", discord::auth::clientID.toStdString()},
-                      rpp::BodyItem{"client_secret", discord::auth::clientSecret.toStdString()}, rpp::BodyItem{"grant_type", "authorization_code"},
-                      rpp::BodyItem{"code", code}, rpp::BodyItem{"redirect_uri", discord::url::redirect.toStdString()},
-                      rpp::BodyItem{"scope", discord::auth::scope.toStdString()}});
-
-            rpp::Response res = req.post(body);
+            body.add({rpp::Body_Item{"client_id", discord::auth::clientID.toStdString()},
+                      rpp::Body_Item{"client_secret", discord::auth::clientSecret.toStdString()}, rpp::Body_Item{"grant_type", "authorization_code"},
+                      rpp::Body_Item{"code", code}, rpp::Body_Item{"redirect_uri", discord::url::redirect.toStdString()},
+                      rpp::Body_Item{"scope", discord::auth::scope.toStdString()}});
+            rpp::Request req;
+            req.set_verify_ssl(false);
+            req.set_verbose(true);
+            rpp::Response res = req.post(discord::url::token.toStdString(), body);
             qDebug() << "[" << res.status << "] " << QString::fromStdString(res.text);
         } else {
             // Handle errors
         }
 
-        this->close();
+        close();
     }
 }
